9347f24a92c2f5c74c131d5f39e4ab60c1675583
CrisCaleb <cristhian.cepeda@uab.edu.bo>
2025-09-24 22:17:34 -0400

    proyecto terminado done (#1)
    Co-authored-by: Criscaleb <cristhianc@outlook.com>

--- lab-laravel-mysql/routes/api.php
+++ lab-laravel-mysql/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CatController;
+use App\Http\Controllers\DogController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +12,12 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/cats', [CatController::class, 'get']);
 Route::post('/cats', [CatController::class, 'store']);
+Route::get('/cats/{id}', [CatController::class, 'show']);
+Route::put('/cats/{id}', [CatController::class, 'update']);
+Route::delete('/cats/{id}', [CatController::class, 'destroy']);
+
+Route::get('/dogs', [DogController::class, 'get']);
+Route::post('/dogs', [DogController::class, 'store']);
+Route::get('/dogs/{id}', [DogController::class, 'show']);
+Route::put('/dogs/{id}', [DogController::class, 'update']);
+Route::delete('/dogs/{id}', [DogController::class, 'destroy']);
